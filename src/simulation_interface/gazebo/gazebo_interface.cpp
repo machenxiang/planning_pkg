561980798ceccf3731d64100c0fb6003018264d7
machenxiang <mcx1243858461@gmail.com>
2024-10-17 14:56:31 +0800

    add some sub

--- src/simulation_interface/gazebo/gazebo_interface.cpp
+++ src/simulation_interface/gazebo/gazebo_interface.cpp
@@ -4,12 +4,42 @@ GazeboInterface::GazeboInterface(ros::NodeHandle& nh) {
   nh_ = nh;
   goal_sub_ = nh_.subscribe("/move_base_simple/goal", 10,
                             &GazeboInterface::GoalCallBack, this);
+  path_sub_ =
+      nh_.subscribe("/rda_sub_path", 10, &GazeboInterface::PathCallBack, this);
+  laser_scan_sub_ = nh_.subscribe("/limo/scan", 10,
+                                  &GazeboInterface::LaserScanCallBack, this);
+  obs_sub_ = nh_.subscribe("/rda_obstacles", 10,
+                           &GazeboInterface::GazeboObsCallBack, this);
+
+  //                             rospy.Subscriber("/rda_sub_path", Path,
+  //                             self.path_callback)
+  // self.listener = tf.TransformListener()
+
+  // # Topic Subscribe
+  // if not use_scan_obstacle:
+  //     rospy.Subscriber("/rda_obstacles", ObstacleArrayMsg,
+  //     self.obstacle_callback)
+  // else:
+  //     rospy.Subscriber("/scan", LaserScan, self.scan_callback)
 }
 
 void GazeboInterface::GoalCallBack(const geometry_msgs::PoseStamped& goal) {
-  AINFO << "bug1";
   AINFO << goal.pose.position.x << "," << goal.pose.position.y;
 }
 
 void GazeboInterface::SpinOnce() { ros::spinOnce(); }
+
+void GazeboInterface::PathCallBack(const nav_msgs::Path& path) {
+  for (const auto iter : path.poses) {
+    AINFO << iter.pose.position.x << "," << iter.pose.position.y;
+  }
+}
+void GazeboInterface::LaserScanCallBack(
+    const sensor_msgs::LaserScan& laser_scan) {
+  AINFO << laser_scan.angle_min << "," << laser_scan.angle_max;
+}
+
+void GazeboInterface::GazeboObsCallBack(
+    const costmap_converter::ObstacleArrayMsg& obs) {}
+
 }  // namespace gazebo_interface
--- src/simulation_interface/gazebo/gazebo_interface.h
+++ src/simulation_interface/gazebo/gazebo_interface.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
 
+#include "costmap_converter/ObstacleArrayMsg.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "log.h"
+#include "nav_msgs/Path.h"
 #include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
 namespace gazebo_interface {
 
 class GazeboInterface {
@@ -17,9 +20,15 @@ class GazeboInterface {
  private:
   ros::NodeHandle nh_;
   ros::Subscriber goal_sub_;
+  ros::Subscriber path_sub_;
+  ros::Subscriber laser_scan_sub_;
+  ros::Subscriber obs_sub_;
 
  private:
   void GoalCallBack(const geometry_msgs::PoseStamped& goal);
+  void PathCallBack(const nav_msgs::Path& path);
+  void LaserScanCallBack(const sensor_msgs::LaserScan& laser_scan);
+  void GazeboObsCallBack(const costmap_converter::ObstacleArrayMsg& obs);
 };
 
 }  // namespace gazebo_interface
